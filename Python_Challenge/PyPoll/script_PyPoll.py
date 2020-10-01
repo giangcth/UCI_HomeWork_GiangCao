15207bc9329a8d0085c764e3889e3d9d4a84a310
Giang Cao <gicao@pafco.net>
2020-09-30 20:41:01 -0700

    HW python

---
+++ Python_Challenge/PyPoll/script_PyPoll.py
@@ -0,0 +1,102 @@
+import os
+import csv
+import sys
+
+
+
+PyPoll_csv = os.path.join('Pypoll','Resources','election_data.csv')
+
+totalvoters = 0
+count = 0
+percent = 0
+
+candidates = []
+countcandidate = []
+percentcandidate = []
+
+with open(PyPoll_csv, 'r') as csvfile:
+
+    reader = csv.reader(csvfile, delimiter=',')
+    header = next(reader)
+    
+    for row in reader:
+        totalvoters +=1
+        if row[2] not in candidates:
+           candidates.append(row[2])
+    
+    
+    for i in candidates:
+        count = 0
+        percent = 0
+        
+        csvfile.seek(0)
+        next(reader)
+
+        for row in reader:
+            if i == row[2]:
+                
+                count += 1
+                percent = round((count / totalvoters) * 100)
+                    
+        countcandidate.append(count)
+        percentcandidate.append(percent)
+
+roster=zip(candidates, percentcandidate, countcandidate)
+
+maxpercent = max(percentcandidate)
+winnerindex =percentcandidate.index(maxpercent)
+winner = candidates[winnerindex]
+
+#print result
+
+print(f"ELECTION RESULTS")
+print(f"-----------------------------------------------")
+
+for c, p, o in roster:
+    print(f'{c} : {p} % ({o})')
+
+print(f"-----------------------------------------------")
+
+print("The Winner: " + winner)
+print(f"-----------------------------------------------")
+
+
+# Write the output file
+
+output_PyBank = os.path.join("PyBank.txt")
+
+with open(output_PyBank, "w") as txtfile:
+
+    txtfile.write(f"ELECTION RESULTS \n"),
+    txtfile.write(f"------------------------------------------------------------- \n"),
+    txtfile.write("Total Votes: " + str(totalvoters) + " \n")
+    
+    roster_string = [f'{key} : {roster[key]}' for key in roster]
+    for st in roster_string:
+        txtfile.writelines(f'{st}\n'),
+
+    txtfile.write(f"------------------------------------------------------------- \n"),
+    
+    txtfile.write("The Winner: " + str(winner)+ " \n"),
+    
+    txtfile.write(f"------------------------------------------------------------- \n")
+
+txtfile.write.close()  
+   
+
+ 						
+						
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
