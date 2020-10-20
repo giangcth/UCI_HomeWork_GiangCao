0c8494b9c7e7497f24051a427cc6d1f991574f44
giangcth <giangcth@icloud.com>
2020-10-19 17:10:40 -0700

    HW API

---
+++ Python_API Challenge/api_keys.py
@@ -0,0 +1,12 @@
+# OpenWeatherMap API Key
+weather_api_key = "74108051b9432caff3e3b31944cb60e8"
+
+# Google API Key
+
+gkey = "AIzaSyDVcwvwzsuJshT2wQOxVKMZPpUxKcEaP5E"
+
+census_key = "8b48391c304f4c3504ef8292bb63abc0596e8e14"
+
+API_key ="5288a346"
+
+fbi_key = "ScD28AGlhbn8iQy1qrUV48adlUu18Y4aTnBU1UJ6"
