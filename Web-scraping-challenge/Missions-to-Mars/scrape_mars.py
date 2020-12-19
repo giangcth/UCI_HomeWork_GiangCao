0daf739403de343404a63422ba3f57e26f70deef
giangcth <giangcth@icloud.com>
2020-12-19 10:11:47 -0800

    All

---
+++ Web-scraping-challenge/Missions-to-Mars/scrape_mars.py
@@ -0,0 +1,107 @@
+from splinter import Browser
+from pprint import pprint
+import pymongo
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+import cssutils
+from flask import Flask, render_template
+import time
+import numpy as np
+import json
+from selenium import webdriver
+
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    return Browser("chrome", **executable_path, headless=False)
+
+
+def scrape():
+    browser = init_browser()
+
+    mars_collection = {}
+
+
+     # MARS NEWS URL
+    url1 = "https://mars.nasa.gov/news/"
+    browser.visit(url1)
+
+    time.sleep(1)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup1 = BeautifulSoup(html, 'html.parser')
+
+    # loop over results to get article titles and bodies:
+    result = soup1.find("div", class_="list_text")
+
+    # scrape the article title & body: 
+    mars_collection["news_date"] = result.find("div", class_="list_date").text
+    mars_collection["news_title"] = result.find("div", class_="content_title").text
+    mars_collection["news_body"] = result.find("div", class_="article_teaser_body").text
+
+        
+    # MARS FEATURE IMAGE
+    url2 = "https://jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(url2)
+
+    html = browser.html
+    soup2 = BeautifulSoup(html, 'lxml')
+
+    # Loop over to get the full size of Mars' feature image
+    article = soup2.find('article', class_='carousel_item')
+
+    html = f'{article}'
+    soup3 = BeautifulSoup(html)
+    div_style = soup3.find('article')['style']
+    style = cssutils.parseStyle(div_style)
+    url = style['background-image']
+    url = url.replace('url(', '').replace(')', '')
+
+    mars_collection["featured_image_url"] = "https://jpl.nasa.gov" + url
+
+
+    # MARS FACTS
+    url3 = 'https://space-facts.com/mars/#content/tablepress-comp-mars'
+    table = pd.read_html(url3)[0]
+    table.columns = ["Description","Mars"]
+    clean_table = table.set_index(["Description"])
+    mars_table = clean_table.to_html()
+    mars_table = mars_table.replace("\n", "")
+    
+    mars_collection["facts_table"] = mars_table
+
+    # MARS HEMISPHERES
+    url4 = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url4)
+
+    hemisphere_image_urls = []
+    for runtime in range(4):
+        image = browser.find_by_tag('h3')
+        image[runtime].click()
+        time.sleep(2)
+        html = browser.html
+        soup4 = BeautifulSoup(html, 'html.parser')
+
+        src = soup4.find('img', class_='wide-image')["src"]
+        image_title = soup4.find('h2', class_='title').text
+        
+        image_url = ['https://astrogeology.usgs.gov' + src]
+
+        dictionary = {"title": image_title, "img_url": image_url}
+        
+        hemisphere_image_urls.append(dictionary)
+        
+        browser.back()
+
+    # Collection of information
+    mars_collection["hemisphere_image"] = hemisphere_image_urls
+    
+    # Close the browser after scraping
+    browser.quit()
+
+    # Return results
+    return mars_collection
---
+++ javascript-challenge/UFO-level-1/StarterCode/static/js/app.js
@@ -0,0 +1,78 @@
+// from data.js
+var tableData = data;
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+
+// Use d3 to update each cell's text with
+// // UFO Sightings report values (date, City, State, Country, Shape, Duration, Comments)
+tableData.forEach((UFOSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(UFOSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+});
+
+// Console.log the UFO Sightings data from data.js
+console.log(tableData);
+
+// // Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Create and Complete the event handler function for Date Form
+function runEnter() {
+    // clears the data of the current table        
+    tbody.html("");
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    // print "You have just clicked the 'Filter Table' on console, for testing
+    console.log("You have just clicked the ' Date Time Filter Button'.");
+    // Select the input element and get the raw HTML node
+    var inputElementDate = d3.select("#datetime");
+    var inputElementCity = d3.select("#cityname");
+    var inputElementState = d3.select("#statename");
+    var inputElementCountry = d3.select("#countryname");
+    var inputElementShape = d3.select("#shapename");
+
+    // Get the value property of the input element
+    var inputValueDate = inputElementDate.property("value").toLowerCase();
+    var inputValueCity = inputElementCity.property("value").toLowerCase();
+    var inputValueState = inputElementState.property("value").toLowerCase();
+    var inputValueCountry = inputElementCountry.property("value").toLowerCase();
+    var inputValueShape = inputElementShape.property("value").toLowerCase();
+    
+    console.log(inputValueDate);
+    console.log(inputValueCity);
+    console.log(inputValueState);
+    console.log(inputValueCountry);
+    console.log(inputValueShape);
+
+        
+    var filteredData = tableData.filter(UFO => UFO.datetime === inputValueDate &&
+                                        UFO.city === inputValueCity &&
+                                        UFO.state === inputValueState &&
+                                        UFO.country === inputValueCountry &&
+                                        UFO.shape === inputValueShape       
+        );
+
+    console.log(filteredData);
+
+    // Use d3 to update each cell's text with
+    // // UFO Sightings report values (date, City, State, Country, Shape, Duration, Comments)
+    filteredData.forEach((UFOSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(UFOSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
+};
+
